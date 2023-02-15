3dbee330a487101c14cf820b5fb8f240fe2ff29b
ddu0422 <ddu0422@naver.com>
2023-02-15 21:32:02 +0900

    jpa: 3.4 영속성 컨텍스트의 특징

--- src/main/java/per/study/springboot/jpa/JpaMain.java
+++ src/main/java/per/study/springboot/jpa/JpaMain.java
@@ -8,11 +8,16 @@ import javax.persistence.Persistence;
 import per.study.springboot.jpa.entity.JpaMember;
 
 /**
- * 영속성 컨텍스트가 엔티티를 관리할 때 장점
- * 1. 1차 캐시
+ * ### 영속성 컨텍스트가 엔티티를 관리할 때 장점 ###
+ * 1. 1차 캐시 (성능상 이점) => Application Level [REPEATABLE READ] 격리 수준 제공
+ *    - 1차 캐시에 엔티티가 있는 경우 1차 캐시에서 데이터 반환
+ *    - 1차 캐시에 엔티티가 없는 경우 DB 조회 후 1차 캐시 저장 후 데이터 반환
  * 2. 동일성 보장
- * 3. 트랜잭션을 지원하는 쓰기 지연
+ *    - 동일한 인스턴스
+ * 3. 트랜잭션을 지원하는 쓰기 지연 (성능상 이점)
+ *    - 모아둔 쿼리를 데이터베이스에 한번에 전달해서 성능 최적화
  * 4. 변경 감지
+ *    - 스탭샷과 엔티티를 비교해서 변경된 엔티티를 찾은 후 수정 SQL을 쓰기 지연 SQL 저장소에 전달
  * 5. 지연 로딩
  */
 
@@ -30,8 +35,10 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
 
         try {
+            // 엔티티 매니저는 데이터 변경 시 트랜잭션을 시작해야 함.
             tx.begin();
             logic(em);
+            // 커밋하는 순간 데이터베이스에 SQL을 보냄
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -51,12 +58,14 @@ public class JpaMain {
         member.setUsername("두호");
         member.setAge(29);
 
+        // 1차 캐시에 저장
         // 객체를 저장한 상태 (영속)
         // 엔티티 저장시 엔티티 매니저의 persist() 메서드에 저장할 엔티티를 넘겨줌.
         em.persist(member);
         // 엔티티의 값만 변경하면 UPDATE SQL을 생성해서 데이터베이스에 값을 변경함.
         member.setAge(30);
 
+        // 1차 캐시에서 조회
         // 단건 조회
         // find 메서드는 조회활 엔티티 타입과 @Id로 데이터베이스 테이블의 기본 키와 매핑한 식별자 값으로 엔티티 하나를 조회하는 단순한 조회 메서드
         JpaMember findMember = em.find(JpaMember.class, id);
