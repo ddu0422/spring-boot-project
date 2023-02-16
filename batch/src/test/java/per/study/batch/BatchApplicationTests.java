46540ea691ca6c4bdb8fe0abe80f5ab593a6ec5e
ddu0422 <ddu0422@naver.com>
2023-02-16 17:52:43 +0900

    batch: init project

---
+++ batch/src/test/java/per/study/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package per.study.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
