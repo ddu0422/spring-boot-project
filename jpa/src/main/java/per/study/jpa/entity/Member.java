0bcbb419260d23c312255acce830601237892ac5
ddu0422 <ddu0422@naver.com>
2023-04-05 08:26:37 +0900

    jpa: 4.5 DDL 생성 기능

--- jpa/src/main/java/per/study/jpa/entity/Member.java
+++ jpa/src/main/java/per/study/jpa/entity/Member.java
@@ -9,6 +9,7 @@ import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Date;
@@ -22,14 +23,20 @@ import java.util.Date;
  *  none: 자동 생성 기능 사용하지 않음
  */
 @Entity // 이 클래스를 테이블과 매핑한다고 JPA에게 알려줌
-@Table(name = "JPA_MEMBER") // 엔티티 클래스에 매핑할 테이블 정보를 알려줌, 생략하면 엔티티 이름을 테이블 이름으로 매핑
+@Table(
+    name = "JPA_MEMBER", // 엔티티 클래스에 매핑할 테이블 정보를 알려줌, 생략하면 엔티티 이름을 테이블 이름으로 매핑
+    uniqueConstraints = {
+        // 직접 DDL을 만든다면 사용할 이유가 없지만, 애플리케이션 개발자가 엔티티만 보고도 손쉽게 다양한 제약 조건을 파악할 수 있는 장점
+        @UniqueConstraint(name = "NAME_AGE_UNIQUE", columnNames = {"NAME", "AGE"})
+    }
+)
 public class Member {
 
     @Id // 테이블의 기본 키에 매핑 (식별자 필드)
     @Column(name = "ID") // ImprovedNamingStrategy인 경우 테이블 명이나 컬럼 명이 생략되면 "자바의 카멜 표기법을 테이블의 언더스코어 표기법"으로 매핑
     private String id;
 
-    @Column(name = "NAME") // name 속성을 사용해서 필드를 name 속성 값에 해당하는 컬럼에 매핑
+    @Column(name = "NAME", nullable = false, length = 10) // name 속성을 사용해서 필드를 name 속성 값에 해당하는 컬럼에 매핑
     private String username;
 
     // 매핑 정보가 없는 필드 -> 매핑 어노테이션을 생략하면 필드명을 사용해서 컬럼명으로 매핑
