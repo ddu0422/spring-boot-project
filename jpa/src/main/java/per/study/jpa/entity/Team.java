b913627f2a28f4562b06d306ac60529f029ef369
ddu0422 <ddu0422@naver.com>
2023-08-02 15:40:56 +0900

    jpa: 5.3.1 양방향 연관관계 매핑

--- jpa/src/main/java/per/study/jpa/entity/Team.java
+++ jpa/src/main/java/per/study/jpa/entity/Team.java
@@ -3,6 +3,9 @@ package per.study.jpa.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -11,6 +14,9 @@ public class Team {
     @Column(name = "TEAM_ID")
     private String id;
     private String name;
+
+    @OneToMany(mappedBy = "team") // 반대쪽 매핑의 필드 이름을 값으로 설정 (Member클래스 team 확인)
+    private List<Member> members = new ArrayList<>();
 
     protected Team() {}
 
@@ -33,5 +39,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
